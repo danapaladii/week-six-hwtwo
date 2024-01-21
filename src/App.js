2d01ef4eb88c1fa7ff7d22445a9299c20879383c
danapaladii <danapaladii22@gmail.com>
2024-01-20 19:03:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState(0);
+  const {name, description, source, image} = data[city];
+
+  const previousCity = () => {
+    setCity((city => {
+      city --;
+      if (city < 0) {
+        return data.length-1;
+      }
+      return city;
+    }))
+  }
+
+  const nextCity = () => {
+    setCity((city => {
+      city ++;
+      if (city > data.length-1) {
+        city = 0;
+      }
+      return city;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <img src={image} width="500px" alt="city" />
+      </div>
+
+      <div className="container">
+        <h1>{name}</h1>
+      </div>
+
+      <div className="container">
+        <p>{description}</p>
+      </div>
+
+      <div className="container">
+        <h3>Source: {source}</h3>
+      </div>
+
+      <div className="btn container">
+        <button onClick={previousCity}>Previous</button>
+        <button onClick={nextCity}>Next</button>
+      </div>
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const data = [
+
+    {
+        id: 1,
+        name: "London",
+        description: "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.",
+        source: "Google",
+        image: "https://t4.ftcdn.net/jpg/00/88/22/81/240_F_88228188_keoX5q3nNcXtphDbAVEoq7dzGwok5xNf.jpg"
+
+    },
+
+    {
+        id: 2,
+        name: "Washington DC",
+        description: "Washington, DC, the U.S. capital, is a compact city on the Potomac River, bordering the states of Maryland and Virginia. It’s defined by imposing neoclassical monuments and buildings – including the iconic ones that house the federal government’s 3 branches: the Capitol, White House and Supreme Court. It's also home to iconic museums and performing-arts venues such as the Kennedy Center.",
+        source: "Google",
+        image: "https://t4.ftcdn.net/jpg/01/64/29/65/240_F_164296580_EbgcDgbe6MpsU4FBzZ5N918cibbgFxVK.jpg"
+
+    },
+
+    {
+        id: 3,
+        name: "Prague",
+        description: "Prague, capital city of the Czech Republic, is bisected by the Vltava River. Nicknamed “the City of a Hundred Spires,” it's known for its Old Town Square, the heart of its historic core, with colorful baroque buildings, Gothic churches and the medieval Astronomical Clock, which gives an animated hourly show. Completed in 1402, pedestrian Charles Bridge is lined with statues of Catholic saints. ",
+        source: "Google",
+        image: "https://t3.ftcdn.net/jpg/01/06/20/60/240_F_106206022_ImF7YRMDiziKRp5ZkZGdmnjQBi1K2drB.jpg"
+
+    },
+
+    {
+        id: 4,
+        name: "Vienna",
+        description: "Vienna, Austria’s capital, lies in the country’s east on the Danube River. Its artistic and intellectual legacy was shaped by residents including Mozart, Beethoven and Sigmund Freud. The city is also known for its Imperial palaces, including Schönbrunn, the Habsburgs’ summer residence. In the MuseumsQuartier district, historic and contemporary buildings display works by Egon Schiele, Gustav Klimt and other artists. ",
+        source: "Google",
+        image: "https://t4.ftcdn.net/jpg/03/37/63/99/240_F_337639940_88dzqls2bJ8Mw1Fpd2F6FtICfib8dktb.jpg"
+
+    },
+
+    {
+        id: 5,
+        name: "Dresden",
+        description: "Dresden has a long history as the capital and royal residence for the Electors and Kings of Saxony, who for centuries furnished the city with cultural and artistic splendor. The city was known as the Jewel Box, because of its Baroque and Rococo city centre. Dresden's Striezelmarkt is one of the largest Christmas markets in Germany and is considered the first genuine Christmas market in the world. ",
+        source: "Google",
+        image: "https://t3.ftcdn.net/jpg/04/10/08/52/240_F_410085223_DxM4O5gwqhzGhzo94zg6CVXdRJa4rHqu.jpg"
+
+    },
+
+    {
+        id: 6,
+        name: "Tallinn",
+        description: "Tallinn, Estonia’s capital on the Baltic Sea, is the country’s cultural hub. It retains its walled, cobblestoned Old Town, home to cafes and shops, as well as Kiek in de Kök, a 15th-century defensive tower. Its Gothic Town Hall, built in the 13th century and with a 64m-high tower, sits in historic Tallinn’s main square. St. Nicholas Church is a 13th-century landmark exhibiting ecclesiastical art. ",
+        source: "Google",
+        image: "https://t4.ftcdn.net/jpg/02/71/40/33/240_F_271403346_UecVOn1KVWGhkWRWeLrxin0l20RQD6gX.jpg"
+
+    },
+
+    {
+        id: 7,
+        name: "Budapest",
+        description: "Budapest, Hungary’s capital, is bisected by the River Danube. Its 19th-century Chain Bridge connects the hilly Buda district with flat Pest. A funicular runs up Castle Hill to Buda’s Old Town, where the Budapest History Museum traces city life from Roman times onward. Trinity Square is home to 13th-century Matthias Church and the turrets of the Fishermen’s Bastion, which offer sweeping views.",
+        source: "Google",
+        image: "https://t4.ftcdn.net/jpg/03/05/30/99/240_F_305309942_4HHrO3daStxVyEIjOC46P2QvwIKEzZru.jpg"
+
+    }
+    
+]
